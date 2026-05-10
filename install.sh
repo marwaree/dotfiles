131472d81fe4e5abd843e8cf9381331ad1e6547c
Marco Riola Fraga <marcoriola@proton.me>
2026-05-10 13:21:32 +0200

    Added favorite apps to installer

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
 
 echo "Installing yay..."
-sudo pacman -S --needed base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
+sudo pacman -S --no-confirm --needed base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
 
 echo "Installing required packages..."
-sudo pacman -S meson gcc cpio sddm qt6-svg qt6-virtualkeyboard qt6-multimedia-ffmpeg pamixer fzf hyprpolkitagent swww mako rofi nautilus waybar wl-clipboard stow firefox ttf-cascadia-mono-nerd neovim matugen fastfetch zsh hypridle eza zoxide bat impala bluetui hyprlock wget ghostty
+sudo pacman -S --no-confirm meson gcc cpio sddm qt6-svg qt6-virtualkeyboard qt6-multimedia-ffmpeg pamixer fzf hyprpolkitagent awww mako rofi nautilus waybar wl-clipboard stow firefox ttf-cascadia-mono-nerd neovim matugen fastfetch zsh hypridle eza zoxide bat impala bluetui hyprlock wget ghostty
 
-yay -S rmtrash sddm-silent-theme python-pywalfox hyprshot-git
+yay -S --no-confirm rmtrash sddm-silent-theme python-pywalfox hyprshot-git
 
 sudo pywalfox install
 
@@ -66,6 +66,17 @@ n | N | no | No | NO) ;;
 
   info "Theme installed"
   ;;
+esac
+
+read -p "Do you want to install marware's favorite apps? (Y/n): " choice
+
+case "$choice" in
+y | Y | yes | Yes | YES)
+  sudo pacman -S helium-browser-bin steam vesktop-bin prismlauncher libreoffice-fresh qbittorrent gimp localsend inkscape audacity gnome-disk-utility vlc
+  yay -S lunar-client
+  ;;
+n | N | no | No | NO) ;;
+*) ;;
 esac
 
 read -p "Finished installation. Do you want to exit hyprland to apply the changes now? (Y/n): " choice
