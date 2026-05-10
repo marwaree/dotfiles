43f4cdcf63005f0dcc52cb6e1accca82e1736250
Marco Riola Fraga <marcoriola@proton.me>
2026-05-10 12:35:09 +0200

    Adapted keybinds for split keyboard

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -28,25 +28,14 @@ return {
       -- this file can contain specific instructions for your project
       instructions_file = "avante.md",
       -- for example
-      provider = "claude",
+      provider = "ollama",
       providers = {
-        claude = {
-          endpoint = "https://api.anthropic.com",
-          model = "claude-sonnet-4-20250514",
-          timeout = 30000, -- Timeout in milliseconds
-          extra_request_body = {
-            temperature = 0.75,
-            max_tokens = 20480,
-          },
-        },
-        moonshot = {
-          endpoint = "https://api.moonshot.ai/v1",
-          model = "kimi-k2-0711-preview",
-          timeout = 30000, -- Timeout in milliseconds
-          extra_request_body = {
-            temperature = 0.75,
-            max_tokens = 32768,
-          },
+        ollama = {
+          endpoint = "http://192.168.0.74:11434", -- Note that there is no /v1 at the end.
+          model = "glm-5.1:cloud",
+          is_env_set = function()
+            return true
+          end,
         },
       },
     },
--- install.sh
+++ install.sh
@@ -4,11 +4,17 @@ echo "Installing yay..."
 sudo pacman -S --needed base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
 
 echo "Installing required packages..."
-sudo pacman -S sddm qt6-svg qt6-virtualkeyboard qt6-multimedia-ffmpeg pamixer fzf hyprpolkitagent swww mako rofi nautilus waybar wl-clipboard stow firefox ttf-cascadia-mono-nerd neovim matugen fastfetch zsh hypridle eza zoxide bat impala bluetui hyprlock wget ghostty
+sudo pacman -S meson gcc cpio sddm qt6-svg qt6-virtualkeyboard qt6-multimedia-ffmpeg pamixer fzf hyprpolkitagent swww mako rofi nautilus waybar wl-clipboard stow firefox ttf-cascadia-mono-nerd neovim matugen fastfetch zsh hypridle eza zoxide bat impala bluetui hyprlock wget ghostty
 
 yay -S rmtrash sddm-silent-theme python-pywalfox hyprshot-git
 
 sudo pywalfox install
+
+hyprpm update
+
+hyprpm add https://github.com/zjeffer/split-monitor-workspaces # Add the plugin repository
+hyprpm enable split-monitor-workspaces                         # Enable the plugin
+hyprpm reload                                                  # Reload the plugins
 
 mkdir ~/Pictures/Screenshots
 export HYPRSHOT_DIR="~/Pictures/Screenshots"
@@ -18,14 +24,13 @@ chsh -s /usr/bin/zsh
 read -p "Do you want to remove unnecesary packages for the dotfiles? (Y/n): " choice
 
 case "$choice" in
-    y|Y|yes|Yes|YES)
-        sudo pacman -Rns dunst wofi dolphin kitty
-        ;;
-    n|N|no|No|NO)
-        ;;
-    *)
-        sudo pacman -Rns dunst wofi dolphin kitty
-        ;;
+y | Y | yes | Yes | YES)
+  sudo pacman -Rns dunst wofi dolphin kitty
+  ;;
+n | N | no | No | NO) ;;
+*)
+  sudo pacman -Rns dunst wofi dolphin kitty
+  ;;
 esac
 
 stow .
@@ -34,46 +39,43 @@ sudo stow . -t /root
 read -p "Do you want to install the astronaut sddm theme? (Y/n): " choice
 
 case "$choice" in
-    y|Y|yes|Yes|YES)
+y | Y | yes | Yes | YES)
 
-        sudo cp -r ~/.dotfiles/sddm-astronaut-theme /usr/share/sddm/themes/sddm-astronaut-theme/
-        # Configure SDDM
-        echo "[Theme]
+  sudo cp -r ~/.dotfiles/sddm-astronaut-theme /usr/share/sddm/themes/sddm-astronaut-theme/
+  # Configure SDDM
+  echo "[Theme]
         Current=sddm-astronaut-theme" | sudo tee /etc/sddm.conf >/dev/null
 
-        sudo mkdir -p /etc/sddm.conf.d
-        echo "[General]
+  sudo mkdir -p /etc/sddm.conf.d
+  echo "[General]
         InputMethod=qtvirtualkeyboard" | sudo tee /etc/sddm.conf.d/virtualkbd.conf >/dev/null
 
-        info "Theme installed"
-        ;;
-    n|N|no|No|NO)
-        ;;
-    *)
+  info "Theme installed"
+  ;;
+n | N | no | No | NO) ;;
+*)
 
-        sudo cp -r sddm-astronaut-theme /usr/share/sddm/themes/sddm-astronaut-theme/
-        # Configure SDDM
-        echo "[Theme]
+  sudo cp -r sddm-astronaut-theme /usr/share/sddm/themes/sddm-astronaut-theme/
+  # Configure SDDM
+  echo "[Theme]
         Current=sddm-astronaut-theme" | sudo tee /etc/sddm.conf >/dev/null
 
-        sudo mkdir -p /etc/sddm.conf.d
-        echo "[General]
+  sudo mkdir -p /etc/sddm.conf.d
+  echo "[General]
         InputMethod=qtvirtualkeyboard" | sudo tee /etc/sddm.conf.d/virtualkbd.conf >/dev/null
-        
-        info "Theme installed"
-        ;;
+
+  info "Theme installed"
+  ;;
 esac
-
 
 read -p "Finished installation. Do you want to exit hyprland to apply the changes now? (Y/n): " choice
 
 case "$choice" in
-    y|Y|yes|Yes|YES)
-        pkill hyprland
-        ;;
-    n|N|no|No|NO)
-        ;;
-    *)
-        pkill hyprland
-        ;;
+y | Y | yes | Yes | YES)
+  pkill hyprland
+  ;;
+n | N | no | No | NO) ;;
+*)
+  pkill hyprland
+  ;;
 esac
