998a29ef6ae4bca96ab8254b0b2e7285672bceba
Marco Riola Fraga <marcoriola@proton.me>
2026-08-31 14:31:21 +0200

    nvim update

--- .config/nvim/lua/plugins/kanagawa.lua
+++ .config/nvim/lua/plugins/kanagawa.lua
@@ -22,6 +22,11 @@ return {
                     TelescopeBorder = { bg = theme.ui.bg_m1 },
                     TelescopeResultsBorder = { bg = theme.ui.bg_m1 },
                     TelescopePromptBorder = { bg = theme.ui.bg_m1 },
+
+                    Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
+                    PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+                    PmenuSbar = { bg = theme.ui.bg_m1 },
+                    PmenuThumb = { bg = theme.ui.bg_p2 },
                 }
             end,
         },
